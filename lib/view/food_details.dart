6da0558a6082da0ae4eb5c6ea9ea1ae09b22cd44
mebestaca <mebestaca@gmail.com>
2023-03-09 00:51:22 -0700

    added food detail page

---
+++ lib/view/food_details.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../model/recipe.dart';
+
+class FoodDetails extends StatefulWidget {
+  const FoodDetails({Key? key}) : super(key: key);
+
+  @override
+  State<FoodDetails> createState() => _FoodDetailsState();
+}
+
+class _FoodDetailsState extends State<FoodDetails> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    Recipe recipe = ModalRoute.of(context)?.settings.arguments as Recipe;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe.title),
+      ),
+      body: Hero(
+        tag: recipe.id,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(recipe.image)
+            )
+          ),
+        )
+      ),
+    );
+  }
+}
