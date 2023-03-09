3707d0f3efbcd36d3fef7ab00b1835b39ab8d903
mebestaca <mebestaca@gmail.com>
2023-03-08 19:11:54 -0700

    added main page that allows user to add as many ingredients as they want then the app will display the images of the food that has the ingredient

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_finder/keys/keys.dart';
+import 'package:sliver_header_delegate/sliver_header_delegate.dart';
 
+import '../model/recipe.dart';
+import '../services/remote.dart';
 import '../shared/sharedDecoration.dart';
 
 class Home extends StatefulWidget {
@@ -14,85 +18,202 @@ class _HomeState extends State<Home> {
   late List<String> tagList = [];
   final tagController = TextEditingController();
   String tag = "";
-
-  Iterable<Widget> get tagChips{
-    return tagList.map((e) {
-      return Chip(
-        label: Text(e),
-        deleteIcon: const Icon(Icons.close),
-        onDeleted: () {
-          setState(() {
-            tagList.removeWhere((element) => element == e);
-          });
-        },
-      );
-    });
-  }
+  List<Recipe>? recipeList;
+  bool isLoaded = false;
 
   @override
   Widget build(BuildContext context) {
-    
+
+    double tileHeight = MediaQuery.of(context).size.height * 0.30;
+    double tileWidth = MediaQuery.of(context).size.width * .95;
+
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Card(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    controller: tagController,
-                    decoration: fieldStyle.copyWith(
-                      hintText: "ingredients",
-                      labelText: "ingredients"
-                    ),
-                    onChanged: (val) {
-                      tag = val;
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/food_background.jpg"),
+            fit: BoxFit.fill
+          ),
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: FlexibleHeaderDelegate(
+                statusBarHeight: MediaQuery.of(context).padding.top,
+                expandedHeight: 240,
+                background: MutableBackground(
+                  collapsedColor: Colors.lightGreen,
+                  expandedWidget: Image.asset(
+                    'assets/food.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return StatefulBuilder(
+                                builder: (context, setState) {
+
+                                  return AlertDialog(
+                                    scrollable: true,
+                                    content: Column(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(10.0),
+                                          child: TextFormField(
+                                            controller: tagController,
+                                            decoration: fieldStyle.copyWith(
+                                                hintText: "ingredients",
+                                                labelText: "ingredients"
+                                            ),
+                                            onChanged: (val) {
+                                              tag = val;
+                                            },
+                                          ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Expanded(
+                                              child: Padding(
+                                                padding: const EdgeInsets.all(10.0),
+                                                child: SizedBox(
+                                                  width: double.infinity,
+                                                  child: ElevatedButton(
+                                                    onPressed: () async {
+
+                                                      Map<String, String> data = {
+                                                        "ingredients" : tagList.toString(),
+                                                        "apiKey" : apiKey
+                                                      };
+
+                                                      recipeList = await RemoteService().getRecipes(data);
+                                                      if (recipeList != null) {
+                                                        setState(() {
+                                                          isLoaded = true;
+                                                          Navigator.pop(context);
+                                                        });
+                                                      }
+                                                    },
+                                                    child: const Text("Search"),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              child: Padding(
+                                                padding: const EdgeInsets.all(10.0),
+                                                child: SizedBox(
+                                                  width: double.infinity,
+                                                  child: ElevatedButton(
+                                                    onPressed: () {
+                                                      setState(() {
+                                                        if (tag.isNotEmpty) {
+                                                          tagList.add(tag);
+                                                          tagList = tagList.toSet()
+                                                              .toList();
+                                                          tagController.clear();
+                                                        }
+                                                      });
+                                                    },
+                                                    child: const Text("Add"),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const Divider(
+                                          height: 2,
+                                          thickness: 2,
+                                        ),
+                                        Visibility(
+                                          visible: tagList.isNotEmpty,
+                                          child: Wrap(
+                                            children: tagList.map((e) {
+                                              return Chip(
+                                                label: Text(e),
+                                                deleteIcon: const Icon(Icons.close),
+                                                onDeleted: () {
+                                                  setState(() {
+                                                    tagList.removeWhere((element) => element == e);
+                                                  });
+                                                },
+                                              );
+                                            }).toList(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                }
+                            );
+                          }
+                      );
                     },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          tagList.add(tag);
-                          tagController.clear();
-                        });
-                      },
-                      child: const Text("Add"),
-                    ),
+                ],
+                children: [
+                  FlexibleTextItem(
+                    text: 'Recipe Finder',
+                    collapsedStyle: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.white),
+                    expandedStyle: Theme.of(context)
+                        .textTheme
+                        .headlineMedium
+                        ?.copyWith(color: Colors.white),
+                    expandedAlignment: Alignment.bottomLeft,
+                    collapsedAlignment: Alignment.center,
+                    expandedPadding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                   ),
-                ),
-                const Divider(
-                  height: 2,
-                  thickness: 2,
-                ),
-                Wrap(
-                  children: tagChips.toList(),
-                ),
-                Visibility(
-                  visible: tagList.isNotEmpty,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-
-                          });
-                        },
-                        child: const Text("Search"),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            Visibility(
+              visible: isLoaded,
+              replacement: SliverToBoxAdapter(
+                child: Container(),
+              ),
+              child: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                      String networkImage = recipeList?[index].image ?? "";
+                      return Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/food_background.jpg"),
+                              fit: BoxFit.fill
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Center(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(networkImage),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                              height: tileHeight,
+                              width: tileWidth,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: recipeList?.length
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
