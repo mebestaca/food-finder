edfc052e0ef4a259cfc4c8e8f738e457ce410e7f
mebestaca <mebestaca@gmail.com>
2023-03-09 00:53:17 -0700

    added shared theme

---
+++ lib/shared/sharedTheme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+var mainTheme = ThemeData(
+  primarySwatch: Colors.lightGreen,
+  appBarTheme:const AppBarTheme(
+    iconTheme: IconThemeData(color: Colors.white),
+    foregroundColor: Colors.white,
+  )
+);
